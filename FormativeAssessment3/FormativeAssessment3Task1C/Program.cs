8bc99b73a9a05d248377b9bff43bcf831348484c
Darren Burton <764702111@nzse.ac.nz>
2022-08-25 01:54:45 +1200

    Revert "Revert "update""
    This reverts commit 93608cfee6ad5bdb31fd71036616ac080f1c814d.

--- FormativeAssessment3/FormativeAssessment3Task1C/Program.cs
+++ FormativeAssessment3/FormativeAssessment3Task1C/Program.cs
@@ -116,7 +116,7 @@ namespace FormativeAssessment3Task1C
                 if (method == string.Empty)
                 {
                     listylist.Palindrome();   //palindrome function call 
-                    break;
+                    return;
                 }
                 listylist.Add();    //calls add function to insert user input into node
             }
